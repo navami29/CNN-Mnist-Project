12507ee5f88f2f4b6eb1d37b19af85c37c04af29
Navami J Nair <navamijnair2003@gmail.com>
2025-11-19 02:31:54 +0000

    changes

--- src/train_model.py
+++ src/train_model.py
@@ -2,3 +2,21 @@ from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+print(f"load mnist dataset")
+#load of mnist
+(X_train, y_train),(X_test, y_test) = keras.datasets.mnist.load_data()
+
+#normalizing of the data
+X_train = X_train.astype("float32")/255.0
+X_test = X_test.astype("float32")/255.0
+
+#reshaping of data to CNN
+X_train = X_train.reshape(-1,28,28,1)
+X_test = X_test.reshape(-1,28,28,1)
+
