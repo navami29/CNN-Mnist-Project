c00e87a51c9e8382be441f5374a9df3b6ed808be
Navami J Nair <navamijnair2003@gmail.com>
2025-11-19 01:54:26 +0000

    add initial file

---
+++ init_script.sh
@@ -0,0 +1,20 @@
+echo "A script to create, activate and install requiremnet.txt"
+echo "......."
+
+virtualenv venv
+
+echo "Creation of Virtualenv done..."
+echo "activation my env"
+
+source venv/bin/activate
+
+echo "......."
+
+echo "install. requiremnet.txt"
+pip install -r requirement.txt
+
+sleep(2)
+echo "install done"
+echo "Creation Activation and install of library done"
+
+
